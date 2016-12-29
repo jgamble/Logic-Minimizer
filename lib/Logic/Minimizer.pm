1331bbfaf49637ff7ad5da4b4c4e86e6461aab26
John M. Gamble <jgamble@cpan.org>
2016-12-28 20:34:43 -0600

    Add 'primes', 'essentials', 'title', 'columnlist', and 'vars'.
    Check for empty term arrays for minterms, maxterms, and dontcares.
    
    Change break_columnstring() to list_to_terms(); altering the lists creations slightly.

--- lib/Logic/Minimizer.pm
+++ lib/Logic/Minimizer.pm
@@ -5,7 +5,7 @@ use 5.010001;
 use Moose;
 use namespace::autoclean;
 
-use Carp qw(croak);
+use Carp;
 
 use List::Compare::Functional qw(get_intersection);
 
@@ -27,30 +27,33 @@ our $VERSION = '1.00';
 #    cannot be used by itself.
 #
 has 'width' => (
-	isa => 'Int', is => 'ro', required => 1
+	isa => 'Int', is => 'ro', required => 1,
 );
 
 has 'minterms' => (
 	isa => 'ArrayRef[Int]', is => 'rw', required => 0,
-	predicate => 'has_minterms'
+	predicate => 'has_minterms',
 );
 has 'maxterms' => (
 	isa => 'ArrayRef[Int]', is => 'rw', required => 0,
-	predicate => 'has_maxterms'
+	predicate => 'has_maxterms',
 );
 has 'dontcares' => (
 	isa => 'ArrayRef[Int]', is => 'rw', required => 0,
-	predicate => 'has_dontcares'
+	clearer => 'clear_dontcares',
+	predicate => 'has_dontcares',
 );
 has 'columnstring' => (
 	isa => 'Str', is => 'ro', required => 0,
 	predicate => 'has_columnstring',
 	lazy => 1,
-	builder => 'to_columnstring'
+	builder => 'to_columnstring',
 );
-has 'dc' => (
-	isa => 'Str', is => 'rw',
-	default => '-'
+has 'columnlist' => (
+	isa => 'ArrayRef[Int]', is => 'ro', required => 0,
+	predicate => 'has_columnlist',
+	lazy => 1,
+	builder => 'to_columnlist',
 );
 
 has 'algorithm' => (
@@ -59,14 +62,68 @@ has 'algorithm' => (
 );
 
 #
-# Required attribute to return the solution.
+# Optional attributes.
 #
-# The terms that cover the primes needed to solve the
-# truth table.
+has 'title'	=> (
+	isa => 'Str', is => 'rw', required => 0,
+	predicate => 'has_title'
+);
+has 'dc'	=> (
+	isa => 'Str', is => 'rw',
+	default => '-'
+);
+has 'vars'	=> (
+	isa => 'ArrayRef[Str]', is => 'rw', required => 0,
+	default => sub{['A' .. 'Z']}
+);
+
+
 #
-# Covers (the building blocks of the final form of the solution to
-# the equation) is a "lazy" attribute and is calculated when asked
-# for in code or by the user.
+# Prime implicants, essentials, and covers are all "lazy"
+# attributes and calculated when asked for in code or by
+# the user. All are created via builder functions that
+# are provided by the child class.
+#
+
+#
+# The prime implicants. The hash is of the form
+# 'implicant' => [terms that are covered].
+#
+has 'primes' => (
+	isa => 'HashRef', is => 'ro', required => 0,
+	init_arg => undef,
+	reader => 'get_primes',
+	writer => '_set_primes',
+	predicate => 'has_primes',
+	clearer => 'clear_primes',
+	lazy => 1,
+	builder => 'generate_primes',
+);
+
+#
+# The essential prime implicants (for informational
+# purposes only, algorithms generally don't need
+# it for minimizing).
+#
+has 'essentials' => (
+	isa => 'ArrayRef', is => 'ro', required => 0,
+	init_arg => undef,
+	reader => 'get_essentials',
+	writer => '_set_essentials',
+	predicate => 'has_essentials',
+	clearer => 'clear_essentials',
+	lazy => 1,
+	builder => 'generate_essentials',
+);
+
+#
+# The covers are the building blocks of the final form
+# of the solution to the equations and are used to create
+# the text form of the equations.
+#
+# The terms are listed in an array, but as there may be
+# more than one way to cover the solution, it is an array
+# of cover arrays.
 #
 has 'covers' => (
 	isa => 'ArrayRef[ArrayRef[Str]]', is => 'ro', required => 0,
@@ -76,7 +133,7 @@ has 'covers' => (
 	predicate => 'has_covers',
 	clearer => 'clear_covers',
 	lazy => 1,
-	builder => 'generate_covers'
+	builder => 'generate_covers',
 );
 
 #
@@ -92,7 +149,6 @@ sub catch_errors
 {
 	my $self = shift;
 	my $w = $self->width;
-	my $last_idx = (1 << $w) - 1;
 
 	#
 	# Catch errors involving minterms, maxterms, and don't-cares.
@@ -100,15 +156,31 @@ sub catch_errors
 	croak "Mixing minterms and maxterms not allowed"
 		if ($self->has_minterms and $self->has_maxterms);
 
-	if ($self->has_columnstring)
+	#
+	# Does the width make sense?
+	#
+	croak "'width' must be at least 1" if ($w < 1);
+
+	my $last_idx = (1 << $w) - 1;
+
+	if ($self->has_columnstring or $self->has_columnlist)
 	{
-		croak "Other terms are redundant when using the columnstring attribute"
+		croak "Other terms are redundant when using the columnstring or columnlist attributes"
 			if ($self->has_minterms or $self->has_maxterms or $self->has_dontcares);
+		croak "Use only one of the columnstring or columnlist attributes"
+			if ($self->has_columnstring and $self->has_columnlist);
 
-		my $cl = $last_idx + 1 - length $self->columnstring;
+		my $cl = $last_idx + 1 - ($self->has_columnstring)?
+						length $self->columnstring:
+						$#{$self->columnlist};
 
-		croak "Columnstring length is too short by ", $cl if ($cl > 0);
-		croak "Columnstring length is too long by ", -$cl if ($cl < 0);
+		if ($cl != 0)
+		{
+			my $attr = ($self->has_columnlist)? "Columnlist": "Columnstring";
+
+			croak "$attr length is too short by ", $cl if ($cl > 0);
+			croak "$attr length is too long by ", -$cl;
+		}
 	}
 	else
 	{
@@ -117,10 +189,12 @@ sub catch_errors
 		if ($self->has_minterms)
 		{
 			@terms = @{ $self->minterms };
+			croak "Empty 'minterm' array reference." unless (scalar @terms);
 		}
 		elsif ($self->has_maxterms)
 		{
 			@terms = @{ $self->maxterms };
+			croak "Empty 'maxterm' array reference" unless (scalar @terms);
 		}
 		else
 		{
@@ -129,14 +203,23 @@ sub catch_errors
 
 		if ($self->has_dontcares)
 		{
-			my @intersect = get_intersection([$self->dontcares, \@terms]);
-			if (scalar @intersect != 0)
+			my @dcs = @{ $self->dontcares };
+			unless (scalar @dcs)
 			{
-				croak "Term(s) ", join(", ", @intersect),
-					" are in both the don't-care list and the term list.";
+				carp "Empty 'dontcare' array reference";
+				$self->clear_dontcares();
 			}
+			else
+			{
+				my @intersect = get_intersection([\@dcs, \@terms]);
+				if (scalar @intersect != 0)
+				{
+					croak "Term(s) ", join(", ", @intersect),
+						" are in both the don't-care list and the term list.";
+				}
 
-			push @terms, @{$self->dontcares};
+				push @terms, @dcs;
+			}
 		}
 
 		#
@@ -163,18 +246,6 @@ sub catch_errors
 	croak "Not enough variable names for your width" if (scalar @{$self->vars} < $w);
 
 	return 1;
-}
-
-#
-# minmax_bit_terms()
-#
-# Return the list of terms in bit format; either minterms or maxterms.
-#
-sub minmax_bit_terms
-{
-	my $self = shift;
-
-	return ($self->has_min_bits)? @{$self->min_bits}: @{$self->max_bits};
 }
 
 #
@@ -214,22 +285,31 @@ sub to_columnstring
 }
 
 #
-# Take a column string and return array refs usable as parameters for
+# Take a column list and return array refs usable as parameters for
 # minterm, maxterm, and don't-care attributes.
 #
-sub break_columnstring
+sub list_to_terms
 {
 	my $self = shift;
-	my @bitlist = split(//, $self->columnstring);
+	my(@bitlist) = @_;
 	my $x = 0;
 
 	my(@maxterms, @minterms, @dontcares);
 
 	for (@bitlist)
 	{
-		push @minterms, $x if ($_ eq '1');
-		push @maxterms, $x if ($_ eq '0');
-		push @dontcares, $x if ($_ eq $self->dc);
+		if ($_ eq '1')
+		{
+			push @minterms, $x;
+		}
+		elsif ($_ eq '0')
+		{
+			push @maxterms, $x;
+		}
+		else
+		{
+			push @dontcares, $x;
+		}
 		$x++;
 	}
 
@@ -255,9 +335,7 @@ sub extract_algorithm
 
 =head1 NAME
 
-Logic::Minimizer - The parent class of boolean minimizers.
-
-=cut
+Logic::Minimizer - The parent class of Boolean minimizers.
 
 =head1 SYNOPSIS
 
@@ -269,8 +347,9 @@ minimizer package.
     package Algorithm::SomethingNiftyLikeEspresso;
     extends 'Logic::Minimizer';
 
-(C<Logic::TruthTable> requires the Algorithm::SomethingNiftyLikeEspresso
-to have Logic::Minimizer as its base class.)
+(C<Algorithm::SomethingNiftyLikeEspresso> has C<Logic::Minimizer>
+as its base class, which is required if C<Logic::TruthTable> is to
+use it.)
 
 Then, either use the package directly in your program:
 
@@ -298,7 +377,153 @@ or as a algorithm choice in C<Logic::TruthTable>:
         ],
     );
 
-This class provides the methods
+This class provides the attributes and some of the methods for your
+miniizer class.
+
+=head3 Minimizer Attributes
+
+These are the attributes provided by C<Logic::Minimizer> to
+create and reduce the object. Some attributes are required
+to be set from the program, some are optional, and others are
+generated internally.
+
+The attributes required to create the object are:
+
+=over 4
+
+=item 'width'
+
+C<width> is absolutely required -- it tells the object how many
+variables are used in the Boolean equation.
+
+=item 'minterms'
+
+=item 'maxterms'
+
+The set (if using minterms) or unset (if using maxterms) terms in
+the Boolean equation. The choice affects the output of the equation:
+using minterms results in a sum-of-products form of the equation, while
+using maxterms results in a product-of-sums form.
+
+=item 'dontcares'
+
+The terms that don't affect the output of the equation at all; that
+one literally doesn't care about.
+
+=item 'columnstring'
+
+=item 'columnlist'
+
+Alternate ways of providing minterms, maxterms, and don't-care terms.
+Position 0 in the string or list represents the 0th row of the column,
+position 1 represents the next row, and so on. In either case, the
+output is in sum-of-product form.
+
+=back
+
+Some common sense rule apply. If 'columnstring' or 'columnlist' is
+provided, 'minterms', 'maxterms', and 'dontcares' can't be used.
+
+Likewise, 'minterms' and 'maxterms' can not be used together, and
+'dontcares' can't be used by itself.
+
+Some attributes are optional, and are used to changed the look
+of the output.
+
+=over 4
+
+=item 'title'
+
+A name or description of the problem. Useful for tracking
+which object one is working with, if there's more than one.
+
+=item 'dc'
+
+The don't-care character. May be changed for both aesthetic
+and inter-program communication purposes. Used in columnstring
+or columnlist attributes. Also used in output files by
+C<Logic::TruthTable>, and in 'covers' output (see below).
+Defaults to the character '-'.
+
+=item 'vars'
+
+The variable names used to output the Boolean equation. By default,
+uses 'A' to 'Z'. The names to not have to be single characters, e.g.:
+C<vars => ['a1', 'a0', 'b1', 'b0']>
+
+=back
+
+The attributes that are set during the minimizing process.
+
+These are all "lazy" attributes, and calculated when asked
+for in code or by the user. All are created via builder
+functions that are provided by the child class.
+
+=over 4
+
+=item 'primes'
+
+The prime implicants. A hash in the form
+C<'implicant' => [covered terms]>.
+
+=item 'covers'
+
+The covers are the building blocks of the final form
+of the solution to the equation, and are used to create
+the text form of the equation.
+
+The terms are listed in an array, but as there may be
+more than one way to cover the solution, it is an array
+of cover arrays.
+
+=item 'essentials'
+
+The essential prime implicants (for informational purposes
+only, algorithms generally don't need it for minimizing).
+
+=item 'algorithm'
+
+The class of the child object. for example, if the
+class used to solve the set of terms is Algorithm::QuineMcCluskey,
+then 'algorithm' would be set to 'QuineMcCluskey'.
+
+=back
+
+=head3 Minimizer Methods
+
+These are the methods needed for error checking, or to read from or write to the attributes.
+Some are provided by C<Logic::Minimizer>, while others (the builder
+methods of the attributes 'primes', 'covers', and 'essentials') are
+merely method definitions, and are expected to be provided by the
+child class.
+
+=over 4
+
+=item to_columnstring()
+
+Provided by this module. Converts the column into a single string.
+
+=item to_columnlist()
+
+Provided by this module. Converts the column into an array reference.
+
+=item catch_errors
+
+Provided by this module. Finds basic errors such as empty term arrays,
+insufficient variables for the width, terms that don't fit within the
+table size, and so forth.
+
+=item generate_primes()
+
+=item generate_covers()
+
+=item generate_essentials()
+
+The primes, covers, and essentials attributes need methods to create
+those attributes. The child module of C<Logic::Minimizer> should provide these.
+
+=back
+
 
 =head1 AUTHOR
 
