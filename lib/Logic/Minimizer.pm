3311f385dfcecf6973c3e3aec41970474e5de216
John M. Gamble <john.gamble58@gmail.com>
2019-04-17 19:14:34 -0500

    Reformat the POD; add a LaTeX example for variables.

--- lib/Logic/Minimizer.pm
+++ lib/Logic/Minimizer.pm
@@ -499,7 +499,7 @@ or as a algorithm choice in C<Logic::TruthTable>:
 This class provides the attributes and some of the methods for your
 minimizer class.
 
-=head3 Minimizer Attributes
+=head2 Attributes
 
 These are the attributes provided by C<Logic::Minimizer> to
 create and reduce the object. Some attributes are required
@@ -508,37 +508,36 @@ generated internally.
 
 The attributes required to create the object are:
 
-=over 4
 
-=item 'width'
+=head3 'width'
 
 C<width> is absolutely required -- it tells the object how many
 variables are used in the Boolean equation.
 
-=item 'minterms'
+=head3 'minterms'
 
-=item 'maxterms'
+=head3 'maxterms'
 
 The set (if using minterms) or unset (if using maxterms) terms in
 the Boolean equation. The choice affects the output of the equation:
 using minterms results in a sum-of-products form of the equation, while
 using maxterms results in a product-of-sums form.
 
-=item 'dontcares'
+=head3 'dontcares'
 
 The terms that don't affect the output of the equation at all; that
 one literally doesn't care about.
 
-=item 'columnstring'
+=head3 'columnstring'
 
-=item 'columnlist'
+=head3 'columnlist'
 
 Alternate ways of providing minterms, maxterms, and don't-care terms.
 Position 0 in the string or list represents the 0th row of the column,
 position 1 represents the next row, and so on. In either case, the
 output is in sum-of-product form.
 
-=back
+=head2 Using Attributes
 
 Some common sense rules apply. If 'columnstring' or 'columnlist' is
 provided, 'minterms', 'maxterms', and 'dontcares' can't be used.
@@ -549,14 +548,14 @@ Likewise, 'minterms' and 'maxterms' can not be used together, and
 Some attributes are optional, and are used to changed the look
 of the output.
 
-=over 4
+=head2 Optional Attributes
 
-=item 'title'
+=head3 'title'
 
 A name or description of the problem. Useful for tracking
 which object one is working with, if there's more than one.
 
-=item 'dc'
+=head3 'dc'
 
 The don't-care character. May be changed for both aesthetic
 and inter-program communication purposes. Used in columnstring
@@ -564,13 +563,26 @@ or columnlist attributes. Also used in output files by
 C<Logic::TruthTable>, and in 'covers' output (see below).
 Defaults to the character '-'.
 
-=item 'vars'
+=head3 'vars'
 
 The variable names used to output the Boolean equation. By default,
 uses 'A' to 'Z'. The names do not have to be single characters, e.g.:
 C<vars => ['a1', 'a0', 'b1', 'b0']>
 
-=back
+=head3 'get_format_string'
+
+=head3 'set_format_string'
+
+The sprintf-style strings used to write your variables. Two
+formats are needed, one for the normal output of the variable name,
+the other for the complemented form of the variable name.
+
+The the default format is ["%s", "$s'"]. If, for example, you wanted
+a more LaTeX compatible format, you could write
+
+    set_format_string(["%s", "\\overline $s"]);
+
+=head2 'Lazy' Attributes
 
 The attributes that are set during the minimizing process.
 
@@ -578,14 +590,12 @@ These are all "lazy" attributes, and calculated when asked
 for in code or by the user. All are created by builder
 functions that the child class needs to provide.
 
-=over 4
-
-=item 'primes'
+=head3 'primes'
 
 The prime implicants. A hash in the form
 C<'implicant' => [covered terms]>.
 
-=item 'covers'
+=head3 'covers'
 
 The covers are the building blocks of the final form
 of the solution to the equation, and are used to create
@@ -595,20 +605,18 @@ The terms are listed in an array, but as there may be
 more than one way to cover the solution, it is an array
 of cover arrays.
 
-=item 'essentials'
+=head3 'essentials'
 
 The essential prime implicants (for informational purposes
 only, algorithms generally don't need it for minimizing).
 
-=item 'algorithm'
+=head3 'algorithm'
 
 The class of the child object. for example, if the
 class used to solve the set of terms is Algorithm::QuineMcCluskey,
 then 'algorithm' would be set to 'QuineMcCluskey'.
 
-=back
-
-=head3 Minimizer Methods
+=head2 Minimizer Methods
 
 These are the methods needed for error checking, or to read from or
 write to the attributes.
@@ -618,36 +626,34 @@ methods of the attributes 'primes', 'covers', and 'essentials') are
 merely method definitions, and are expected to be provided by the
 child class.
 
-=over 4
-
-=item to_columnstring
+=head3 to_columnstring
 
 Provided by this module. Converts the column into a single string.
 
-=item to_columnlist
+=head3 to_columnlist
 
 Provided by this module. Converts the column into an array reference.
 
-=item catch_errors
+=head3 catch_errors
 
 Provided by this module. Finds basic errors such as empty term arrays,
 insufficient variables for the width, terms that don't fit within the
 table size, and so forth.
 
-=item to_boolean
+=head3 to_boolean
 
 Provided by this module. Converts a set of covers to a boolean equation.
 
-=item to_boolean_term
+=head3 to_boolean_term
 
 Provided by this module. Converts a cover term into a boolean expression.
 Called by C<to_boolean()>.
 
-=item generate_primes
+=head3 generate_primes
 
-=item generate_covers
+=head3 generate_covers
 
-=item generate_essentials
+=head3 generate_essentials
 
 The primes, covers, and essentials attributes need methods to create
 those attributes. The child module of C<Logic::Minimizer> should provide these.
